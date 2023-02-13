236f22af7a67317299894c3d86cb6e0b08884408
Muratali007 <97438347+Muratali007@users.noreply.github.com>
2023-02-13 12:54:42 +0600

    Update main.go

--- main.go
+++ main.go
@@ -1,43 +1,106 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"log"
 	"net/http"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func formHandler(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "ParseFrom() err: %v", err)
-		return
-	}
-	fmt.Fprintf(w, "Post request success")
-	name := r.FormValue("name")
-	address := r.FormValue("address")
-	fmt.Fprintf(w, "Name = %s\n", name)
-	fmt.Fprintf(w, "Address = %s\n", address)
-
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/hello" {
-		http.Error(w, "404 not found ", http.StatusNotFound)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "method not supported", http.StatusNotFound)
-		return
-	}
-	fmt.Fprintf(w, "hello!")
-}
 func main() {
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
-	http.HandleFunc("/form", formHandler)
-	http.HandleFunc("/hello", helloHandler)
+	// Connect to MongoDB
+	client, err := mongo.Connect(nil, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	fmt.Printf("Starting server at port 5555\n")
-	if err := http.ListenAndServe(":5555", nil); err != nil {
+	// Serve the image on a HTTP endpoint
+	http.HandleFunc("/image", func(w http.ResponseWriter, r *http.Request) {
+		// Find the document in the collection
+		coll := client.Database("test").Collection("image")
+		var doc bson.M
+		err := coll.FindOne(nil, bson.M{}).Decode(&doc)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Decode the base64 string and serve it as a JPEG image
+		imgBase64 := doc["image"].(string)
+		img, err := base64.StdEncoding.DecodeString(imgBase64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		w.Header().Set("Content-Type", "image/jpeg")
+		w.Write(img)
+	})
+
+	// Serve the HTML page
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		html := `
+			<html>
+			  <head>
+				<title>Image from MongoDB</title>
+			  </head>
+			  <body>
+				<img src="/image" alt="Image from MongoDB">
+			  </body>
+			</html>
+		`
+		fmt.Fprintln(w, html)
+	})
+
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
 		log.Fatal(err)
 	}
 }
+
+/*to save image to mongodb
+
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	// Connect to MongoDB
+	client, err := mongo.Connect(nil, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Read image file and encode it as base64 string
+	imgFile, err := ioutil.ReadFile("image.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	imgBase64 := base64.StdEncoding.EncodeToString(imgFile)
+
+	// Create a MongoDB document with the image as a string
+	doc := bson.M{
+		"image": imgBase64,
+	}
+
+	// Insert the document into a collection
+	coll := client.Database("test").Collection("images")
+	_, err = coll.InsertOne(nil, doc)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Image saved in MongoDB")
+}
+
+*/
